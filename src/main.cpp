44cfc9ed6f30be5fac0e001fbed8fcc35fb68e1a
Luna Brooker <rbrooker501@gmail.com>
2026-02-19 14:26:40 +0000

    Adds template main.cpp and folder structure to repo

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * Main code for Engineering Project 1.
+ * 
+ * Team: Team 14
+ * Contributors: Luna Brooker, James White
+ * 
+ * Version 0.0.0
+ * Date: 19-02-2026
+ * 
+ */
+
+
+int main() {
+    return 0;
+}
